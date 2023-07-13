cb70bdeff48644785a864245e82d844ebd9d889f
Kajaveaniket <kajaveaniket@gmail.com>
2023-07-13 11:15:26 +0530

    commit2

--- glocery-project/src/main/java/com/glocery/GloceryProjectApplication.java
+++ glocery-project/src/main/java/com/glocery/GloceryProjectApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GloceryProjectApplication {
 
 	public static void main(String[] args) {
-		System.out.println("changes + feature");
+		System.out.println("changes - feature");
 		SpringApplication.run(GloceryProjectApplication.class, args);
 	}
 
